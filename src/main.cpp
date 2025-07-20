622430afc8a537fbd40e0f8869ab10b85e767437
Wágner András <b31ze5@inf.elte.hu>
2025-07-20 20:57:47 +0200

    loading (test) shaders from files

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,7 @@
 #include <glm/ext/matrix_transform.hpp>
 #include <glm/glm.hpp>
 #include <glm/gtc/constants.hpp>
+#include <string>
 #define GLM_ENABLE_EXPERIMENTAL
 #include <glm/gtx/transform.hpp>
 
@@ -18,8 +19,35 @@
 #include <imgui.h>
 
 // STD
-#include <cstdio>
-#include <iostream>
+#include <filesystem>
+#include <fstream>
+#include <vector>
+
+static std::string read_file(const std::filesystem::path &path) {
+  // open file and seek to the end
+  std::ifstream file(path, std::ios::in | std::ios::binary | std::ios::ate);
+  if (!file.is_open())
+    fprintf(stderr, "Error: Could not open %s\n", path.c_str());
+
+  std::ifstream::pos_type bytesize = file.tellg();
+  if (bytesize == std::ifstream::pos_type(-1))
+    fprintf(stderr, "Error: Could not read %s\n", path.c_str());
+
+  file.seekg(0, std::ios::beg); // reset cursor to beginning
+
+  std::vector<char> bytes(bytesize);
+  file.read(bytes.data(), bytesize);
+
+  return std::string(bytes.data(), bytesize);
+}
+
+static void load_shader_from_file(const GLuint shader, const std::filesystem::path &path) {
+  const std::string shader_string = read_file(path);
+  const char *shader_text = shader_string.data();
+  const GLint shader_length = shader_string.length();
+  glShaderSource(shader, 1, &shader_text, &shader_length);
+  glCompileShader(shader);
+}
 
 // TODO delete start
 
@@ -31,26 +59,6 @@ typedef struct Vertex {
 static const Vertex vertices[3] = {{{-0.6f, -0.4f}, {1.f, 0.f, 0.f}},
                                    {{0.6f, -0.4f}, {0.f, 1.f, 0.f}},
                                    {{0.f, 0.6f}, {0.f, 0.f, 1.f}}};
-
-static const char *vertex_shader_text =
-    "#version 330\n"
-    "uniform mat4 MVP;\n"
-    "in vec3 vCol;\n"
-    "in vec2 vPos;\n"
-    "out vec3 color;\n"
-    "void main()\n"
-    "{\n"
-    "    gl_Position = MVP * vec4(vPos, 0.0, 1.0);\n"
-    "    color = vCol;\n"
-    "}\n";
-
-static const char *fragment_shader_text = "#version 330\n"
-                                          "in vec3 color;\n"
-                                          "out vec4 fragment;\n"
-                                          "void main()\n"
-                                          "{\n"
-                                          "    fragment = vec4(color, 1.0);\n"
-                                          "}\n";
 // TODO delete end
 
 static void error_callback(int error, const char *description) {
@@ -105,7 +113,6 @@ int main(void) {
   ImGui_ImplOpenGL3_Init();
 
   // /* Init shader programs */
-  // GLuint simple_program_id = glCreateProgram();
 
   // TODO delete start
   GLuint vertex_buffer;
@@ -114,12 +121,10 @@ int main(void) {
   glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
   const GLuint vertex_shader = glCreateShader(GL_VERTEX_SHADER);
-  glShaderSource(vertex_shader, 1, &vertex_shader_text, NULL);
-  glCompileShader(vertex_shader);
+  load_shader_from_file(vertex_shader, "test.vert");
 
   const GLuint fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
-  glShaderSource(fragment_shader, 1, &fragment_shader_text, NULL);
-  glCompileShader(fragment_shader);
+  load_shader_from_file(fragment_shader, "test.frag");
 
   const GLuint program = glCreateProgram();
   glAttachShader(program, vertex_shader);
@@ -166,8 +171,7 @@ int main(void) {
     // TODO to window change callback
     glm::mat4 view_projection =
         glm::perspective(glm::half_pi<float>(), ratio, 0.0f, 1000.0f) *
-        glm::lookAt(glm::vec3(0.0f, 1.0f, 1.0f),
-										glm::vec3(0.0f),
+        glm::lookAt(glm::vec3(0.0f, 1.0f, 1.0f), glm::vec3(0.0f),
                     glm::vec3(0.0f, 0.0f, 1.0f));
 
     glm::mat4 mvp = view_projection * world;
